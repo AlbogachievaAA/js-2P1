aa31ef0dacab2e3907f64682a239faff34d1a12d
AlbogachievaAA <albogachievaaa24@magas.ithub.ru>
2026-03-24 09:28:42 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
+let a = "привет";
+//       01234
+// console.log(a[2]);
 
-// function names() {
-//     console.log(111);
+// for(let i = a.length - 1; i >= 0; i--){
+//     console.log(a[i]);
     
 // }
-// names()
 
-// let str = 'maga'
-
-// function names2() {
-//     alert(str)
-//     str = 123
-// }
-// names2()
-// alert(str)
-
-//область видимости
-
-// глобальная переменная 
-// let a = 3;
-// function abc(){
-//     a = "abc";
+// function имя(параметры){
+//     ... тело 
 // }
 
-// // локальная переменная 
-
-// function abc2(){
-//     let b = 3;
-//     b = "abc";
+// function showMessage(){
+//     let message = " Hello, I am JavaScript"
+//     console.log(message);
+//      alert(message)
 // }
-// abc2()
+// showMessage()
+// alert(message) тут будет ошибка из-за области видимости 
+
+// let name  = "вася";
+// function showMessage3(){
+//     name3 = "петя"
+//     let message = "привет" + name3;
+//     alert(message)
+// }
+// showMessage3();
 
 
-let a = "Aisha"
-let b = "Albogachieva"
-
-function nameMy(){
-    alert(a)
-    console.log(b);  
+let name4  = "вася";
+function showMessage4(){
+    let message = "привет" ;
+    alert(name4)
 }
-nameMy()
-
+showMessage4();
